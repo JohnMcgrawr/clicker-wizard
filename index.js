b1b8d2f74b138695743096d7bf9318270462934d
John <john_r_lindstrom@outlook.com>
2017-09-07 17:15:40 +0200

    Fixed username function on chat

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
+
 const app = express();
 const server = http.Server(app);
 const io = socketIo(server);
 
-// Serve files from directory public
+ // Serve files from directory public
 app.use(express.static('public'));
 
 // Set up what we are listening to on socket server
 io.on('connection', (socket) => {
-  socket.on('newMessage', (data) => {
-    socket.emit('message', data);
-    socket.broadcast.emit('message', data);
-  });
-});
 
-io.on('connection', function (socket){
-  console.log('a user connected');
-  socket.broadcast.emit('Connected');
-});
+  socket.on('newMessage', (data , username) => {
+     socket.emit('message', data ,username);
+    socket.broadcast.emit('message', data , username);
+    console.log(username);
+  })});
+
 
 server.listen(8080);
--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,21 @@ const sendButton = document.querySelector('.send-button');
 const messageTxt = document.querySelector('.message-txt');
 const chatMessages = document.querySelector('.chat-messages');
 const chatwindow = document.querySelector('.chat');
+let username;
+
 
 sendButton.addEventListener('click', () => {
-  socket.emit('newMessage', messageTxt.value);
+  socket.emit('newMessage', username, messageTxt.value);
  });
 
-socket.on('message', (data) => {
+socket.on('message', (username ,data) => {
 
   const messageItem = document.createElement('li');
-  messageItem.innerText = data;
+  messageItem.innerText = username + ': ' + data;
   chatMessages.appendChild(messageItem);
   chatwindow.scrollTop = chatwindow.scrollHeight;
-
  });
+
+document.addEventListener('DOMContentLoaded', () =>  {
+ username = prompt('Enter chat-name');
+});
