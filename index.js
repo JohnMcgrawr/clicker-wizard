2c72a0554daa33c3090d06c6bb87215229724471
John <john_r_lindstrom@outlook.com>
2017-09-11 15:03:39 +0200

    Crashed chat but fixed first skill

--- index.js
+++ index.js
@@ -4,25 +4,17 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.Server(app);
 const io = socketIo(server);
-//server talks with all clients
+ //server talks with all clients
  // Serve files from directory public
 app.use(express.static('public'));
 
 // Set up what we are listening to on socket server
 io.on('connection', (socket) => {
-
-  socket.on('newMessage', (data , username , counter) => {
-     socket.emit('message', data ,username, counter);
-    socket.broadcast.emit('message', data , username , counter);
+  socket.on('newMessage', (data , username , cookies) => {
+     socket.emit('message', data ,username, cookies);
+    socket.broadcast.emit('message', data , username  , cookies);
 
   })});
-
-
-//   socket.on('loggedIn', (username) => {
-//     socket.broadcast.emit('loggedIn', username);
-//   })
-// });
-
 
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -7,57 +7,62 @@ const messageTxt = document.querySelector('.message-txt');
 const chatMessages = document.querySelector('.chat-messages');
 const chatwindow = document.querySelector('.chat');
 const autoClickButton = document.querySelector('.spell-1');
-
-
 let username;
-var initDamage = 1;
-let counter = 0;
-let autoclickness;
-var add = (function () {
 
-        return function () {return counter += initDamage;}
-})();
 
-// autoClickButton.addEventListener('click', () => {
-// autoclickness = 1000;
-// counter += autoclickness;
-// console.log('JOHAN ÄR SÅ JÄVLA GRYM! OM DET HÄR FUNKAR BJUDER JAG PÅ EN DIRTY LATTE!!!!');
-// });
 
-sendButton.addEventListener('click', () => {
-  socket.emit('newMessage', username, counter,  messageTxt.value);
-  console.log('fires???');
- });
 
- socket.on('message', (username ,data, counter) => {
-   const messageItem = document.createElement('li');
-   messageItem.innerText = username + ': ' + data +': ' + counter;
-   chatMessages.appendChild(messageItem);
-   chatwindow.scrollTop = chatwindow.scrollHeight;
-  });
 
-messageTxt.addEventListener("keydown", function (e) {
-  if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-  socket.emit('newMessage', username, counter,  messageTxt.value);
-  messageTxt.value = '';
- }
-});
+
+  myAudio = new Audio('/sounds/epic_sax_guy.mp3');
+  myAudio.addEventListener('ended', function() {
+      this.currentTime = 0;
+    this.play();
+  }, false);
+  myAudio.play();
+
+  function playAudio() {
+      var audio = document.getElementById('myAudio');
+      if (audio.paused) {
+          audio.play();
+      }
+      else{
+          audio.pause();
+          audio.currentTime = 0
+      }
+  }
+
 
 document.addEventListener('DOMContentLoaded', () =>  {
  username = prompt('Enter chat-name');
 });
 
-// socket.on('loggedIn', (username) => {
-//   console.log(username);
-// });
+sendButton.addEventListener('click', () => {
+  console.log('trycker på send');
+  socket.emit('newMessage', username, cookies,  messageTxt.value);
+});
 
-// let counter = 0;
-// var add = (function () {
-//
-//         return function () {return counter += initDamage;}
-// })();
+
+ socket.on('message', (username ,data, counter) => {
+   const messageItem = document.createElement('li');
+   messageItem.innerText = username + ': ' + data + ': ' + cookies;
+   chatMessages.appendChild(messageItem);
+   chatwindow.scrollTop = chatwindow.scrollHeight;
+  });
+
+messageTxt.addEventListener("keydown", function (e) {
+  console.log('skickar med enterVafaaan');
+
+   if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+  socket.emit('newMessage', username, cookies,  messageTxt.value);
+  messageTxt.value = '';
+  console.log('skickar med enterVafaaan');
+
+  }
+});
+
+
 
 function myFunction(){
-
     document.getElementById("demo").innerHTML = add();
  }
