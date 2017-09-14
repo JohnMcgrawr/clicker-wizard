97501edfb08a807e50f53345b2aaf842e8ff4a14
johsjo <johsjo83@gmail.com>
2017-09-14 21:31:43 +0200

    4 connect data share attack (#4)
    * Fix merge
    
    * Move attack code form data-share.js to attack.js

--- public/js/clicker-wizard.data.share.js
+++ public/js/clicker-wizard.data.share.js
@@ -1,42 +1,17 @@
-<<<<<<< HEAD
-const couterPoint = document.querySelector('#counter');
-=======
-const couterPoint = document.querySelector('#cookies');
->>>>>>> 6dfb76cf885e06fcd0bbdb19cd94755bc0fc761b
+
+const counterPoint = document.querySelector('#counter-window');
+
 let players = [];
 
-const target = '';
-const testButtonForMassageAttack = document.querySelector('#testAttack');
-
-const sendAttack = () => {
-  socket.emit('attack', 'skill', socket.id, target);
-};
-
-testButtonForMassageAttack.addEventListener('click', () => {
-  sendAttack();
-});
-
-socket.on('attack', (skill, id, target) => {
-  // Uppcomming skills
-  console.log(`${'Nu Vart du Attackerad!! ' + ' , '}${skill} , ${id} , ${target}`);
-  switch (expression) {
-    case 'skill1':
-      // DO COOL STUFF
-      break;
-    case 'skill2':
-      // DO COOL STUFF
-      break;
-    case 'skill3':
-      // DO COOL STUFF
-      break;
-    default:
-  }
-});
 
 socket.on('users', (playersServer) => {
   players = playersServer;
 });
 // ('update', (id, name, score)
 const sendUpdate = () => {
-  socket.emit('update', socket.id, username, parseInt(couterPoint.innerHTML));
+  socket.emit('update', socket.id, username, parseInt(counterPoint.innerHTML));
 };
+
+window.setInterval(() => {
+  sendUpdate();
+}, 500);
--- public/js/clicker-wizard.skills.attack.js
+++ public/js/clicker-wizard.skills.attack.js
@@ -1,1 +1,32 @@
 
+
+let target = '';
+const testButtonForMassageAttack = document.querySelector('#testAttack');
+
+
+let skillType = 'skill';
+const sendAttack = () => {
+  socket.emit('attack', skillType, socket.id, target);
+};
+
+
+testButtonForMassageAttack.addEventListener('click', () => {
+  sendAttack();
+});
+
+socket.on('attack', (skill, id, target) => {
+  // Uppcomming skills
+  console.log(`${'Nu Vart du Attackerad!! ' + ' , '}${skill} , ${id} , ${target}`);
+  switch (skill) {
+    case 'skill':
+      prompt('AAAATTAAAACKERAAADDD!!!!');
+      break;
+    case 'skill2':
+      // DO COOL STUFF
+      break;
+    case 'skill3':
+      // DO COOL STUFF
+      break;
+    default:
+  }
+});
